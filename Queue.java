d05d31771bce170a5f277ea46eaf173504df9609
tafman2002 <72532930+tafman2002@users.noreply.github.com>
2021-01-13 01:48:31 -0500

    Update Queue.java

--- Queue.java
+++ Queue.java
@@ -24,19 +24,31 @@ public class Queue {
      * @return Removed element
      */
     public int dequeue () {
-        int rc = -1;
-        for (int i = 0; i < qSize-1;i++) {
-            int temp = s1.pop();
-            s2.push(temp);
-        }
+        int rc;
+        if (qSize == 0) {
+            System.out.println("Nothing to Dequeue");
+            rc = -1;
+        } else {
+            // Since a Queue has a FIFO structure, all the items except the first
+            // in Stack 1 are popped and placed temporarily into Stack 2
+            for (int i = 0; i < qSize-1;i++) {
+                int temp = s1.pop();
+                s2.push(temp);
+            }
 
-        rc = s1.pop();
+            // Removes the last element in the first stack to return it at the end
+            // of the code, successfully dequeuing the front item
+            rc = s1.pop();
 
-        for (int i = 0; i < qSize-1; i++) {
-            int temp = s2.pop();
-            s1.push(temp);
+            // All the rest of the items placed in Stack 2 are popped and placed back into
+            // Stack 1, retaining the same sequence order. The Queue size is then decremented
+            // to reflect the change in size.
+            for (int i = 0; i < qSize-1; i++) {
+                int temp = s2.pop();
+                s1.push(temp);
+            }
+            qSize--;
         }
-        qSize--;
         return rc;
     }
 
